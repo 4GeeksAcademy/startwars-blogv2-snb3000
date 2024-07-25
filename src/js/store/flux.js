3095936a8d969637bc5deef66d868133feb07eb8
Shafik Bahou <snb3000@gmail.com>
2024-07-25 03:41:52 +0000

    new build with mentor

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,43 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  let apiUrl = "https://swapi.dev/api/";
+  return {
+    store: {
+      characters: [],
+      planets: [],
+      starships: [],
+      favorites: [],
+    },
+    actions: {
+      getCharacters: () => {
+        fetch(apiUrl + "people")
+          .then((resp) => resp.json())
+          .then((data) => setStore({ characters: data.results }))
+          .catch((error) => console.log(error));
+      },
+      getPlanets: () => {
+        fetch(apiUrl + "planets")
+          .then((resp) => resp.json())
+          .then((data) => setStore({ planets: data.results }))
+          .catch((error) => console.log(error));
+      },
+      getStarships: () => {
+        fetch(apiUrl + "starships")
+          .then((resp) => resp.json())
+          .then((data) => setStore({ starships: data.results }))
+          .catch((error) => console.log(error));
+      },
+      getFavorites: (favItem) => {
+        const store = getStore();
+        store.favorites.push(favItem);
+        setStore(store);
+      },
+      deleteFavorites: (index) => {
+        const store = getStore();
+        store.favorites.splice(index, 1);
+        setStore(store);
+      },
+    },
+  };
 };
 
 export default getState;
